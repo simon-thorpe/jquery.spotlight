1c7652b76553d415c116aa50e30a4f2fa759d1cf
Simon Thorpe <simon@hivetechnology.com.au>
2014-09-06 11:10:25 +1000

    fix jquery live method

--- jquery.spotlight.js
+++ jquery.spotlight.js
@@ -63,7 +63,7 @@
 			}
 			
 			// Set up click to close
-			spotlight.live(settings.exitEvent, function(){
+			spotlight.on(settings.exitEvent, function(){
 				if(settings.animate){
 					spotlight.animate({opacity: 0}, settings.speed, settings.easing, function(){
 						if(currentPos == 'static') element.css('position', 'static');
